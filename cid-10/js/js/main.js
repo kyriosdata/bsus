e976dd803abeca533a5cdd383ad271703d45d139
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-04-08 16:13:24 -0300

    cid10

---
+++ cid-10/js/js/main.js
@@ -0,0 +1,21 @@
+function readSingleFile(e) {
+    console.log(e.target.files[0]);
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        displayContents(contents);
+    };
+    reader.readAsText(file);
+}
+
+function displayContents(contents) {
+    var element = document.getElementById('file-content');
+    element.innerHTML = contents;
+}
+
+document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
