07731288e5bebe2e20582b3be82ea1f82bf4d94f
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-04-22 09:12:07 -0300

    preprocessador

--- cid-10/java/preprocessor/src/main/java/com/github/kyriosdata/bsus/cid10/preprocessor/Main.java
+++ cid-10/java/preprocessor/src/main/java/com/github/kyriosdata/bsus/cid10/preprocessor/Main.java
@@ -12,45 +12,25 @@ package com.github.kyriosdata.bsus.cid10.preprocessor;
 import com.google.gson.Gson;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * Aplicativo que visa produzir versão mais compacta da CID-10 e
  * pronta para agilizar a consulta.
  */
 public class Main {
-    public static void main(String[] args) {
-        String file = new Main().getFile("CID-10-CAPITULOS.JSON");
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "CID-10-CAPITULOS.JSON";
 
         Gson gson = new Gson();
-        Capitulos capitulos = gson.fromJson(file, Capitulos.class);
-
-        System.out.println(capitulos.CATFIM.length);
-    }
-
-    private String getFile(String fileName) {
-
-        StringBuilder result = new StringBuilder("");
 
         //Get file from resources folder
-        ClassLoader classLoader = getClass().getClassLoader();
+        ClassLoader classLoader = new Main().getClass().getClassLoader();
         File file = new File(classLoader.getResource(fileName).getFile());
 
-        try (Scanner scanner = new Scanner(file)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                result.append(line).append("\n");
-            }
-
-            scanner.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return result.toString();
+        Capitulos cs = gson.fromJson(new FileReader(file), Capitulos.class);
+        System.out.println(cs.CATFIM.length);
 
     }
 }
