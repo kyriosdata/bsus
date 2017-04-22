20700a782360e5dbaf73961415fd3b5d1af6d9a7
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-04-22 09:47:37 -0300

    tolower

--- cid-10/java/preprocessor/src/main/java/com/github/kyriosdata/bsus/cid10/preprocessor/Main.java
+++ cid-10/java/preprocessor/src/main/java/com/github/kyriosdata/bsus/cid10/preprocessor/Main.java
@@ -14,6 +14,9 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Aplicativo que visa produzir versão mais compacta da CID-10 e
@@ -41,6 +44,16 @@ public class Main {
     }
 
     public void exibeAnalise(Capitulos caps) {
-        System.out.println(caps.CATFIM.length);
+        System.out.println("Total entradas: " + caps.CATFIM.length);
+    }
+
+    public Map<String, List<Integer>> montaDicionario(String[] sentencas) {
+        Map<String, List<Integer>> mapa = new HashMap<>();
+
+        for(int i = 0; i < sentencas.length; i++) {
+            String[] palavras = sentencas[i].split(" ");
+        }
+
+        return mapa;
     }
 }
