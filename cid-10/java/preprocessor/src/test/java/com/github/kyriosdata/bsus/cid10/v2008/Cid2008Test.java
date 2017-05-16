59d0f9691eec249abc6c1a5f1479e5b7f38b1934
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-05-16 18:45:24 -0300

    contrato

---
+++ cid-10/java/preprocessor/src/test/java/com/github/kyriosdata/bsus/cid10/v2008/Cid2008Test.java
@@ -0,0 +1,32 @@
+package com.github.kyriosdata.bsus.cid10.v2008;
+
+import org.junit.Test;
+
+/**
+ * Experimentação das consultas e retornos estabelecidos para busca
+ * na CID-10.
+ *
+ * TODO Deve ser validado por profissional de saúde.
+ */
+public class Cid2008Test {
+
+    @Test
+    public void consultaPorCodigosCompletosExistentes() {
+
+    }
+
+    @Test
+    public void consultaPorVariosCodigosInexistentes() {
+
+    }
+
+    @Test
+    public void consultaPorCodigosParciaisExistentes() {
+
+    }
+
+    @Test
+    public void consultaPorPreposicao() {
+
+    }
+}
