7737f7a817e4a0414a93b296d491107d97a15d7c
kyriosdata <fabio@inf.ufg.br>
2017-06-13 13:17:51 -0300

    tratando entrada

--- cid-10/java/preprocessor/src/test/java/com/github/kyriosdata/bsus/cid10/preprocessor/IndiceTest.java
+++ cid-10/java/preprocessor/src/test/java/com/github/kyriosdata/bsus/cid10/preprocessor/IndiceTest.java
@@ -12,7 +12,33 @@ public class IndiceTest {
 
     @Test
     public void montagemIndice() throws FileNotFoundException {
-        Map<String, Set<String>> idxWords = Indice.montaIndice(Subcategorias.newInstance("cid-10-subcategorias-lower.json").descricao);
-        assertEquals(26 * 26, idxWords.size());
+        final String fileName = "cid-10-subcategorias-lower.json";
+        final Subcategorias sc = Subcategorias.newInstance(fileName);
+
+        Map<String, Set<String>> twoLetters = Indice.montaIndice(sc.descricao);
+        assertEquals(26 * 26, twoLetters.size());
+
+        Set<String> resposta = busca(new String[] { "zy", "ic", "ab", "ic" }, twoLetters);
+        assertEquals(82, resposta.size());
+    }
+
+    public Set<String> busca(String[] criterios, Map<String, Set<String>> idx) {
+
+        // Identifique o menor conjunto (menos buscas)
+        Set<String> menorConjunto = null;
+        int menorTamanho = Integer.MAX_VALUE;
+        for (String criterio : criterios) {
+            Set<String> corrente = idx.get(criterio);
+            if (corrente == null) {
+                continue;
+            }
+
+            if (menorTamanho > corrente.size()) {
+                menorTamanho = corrente.size();
+                menorConjunto = corrente;
+            }
+        }
+
+        return menorConjunto;
     }
 }
