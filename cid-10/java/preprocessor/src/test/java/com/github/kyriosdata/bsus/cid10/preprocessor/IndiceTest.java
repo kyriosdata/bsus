415873869154876719a7c6c52c9b034eff66d27a
kyriosdata <fabio@inf.ufg.br>
2017-06-13 13:04:07 -0300

    tratando entrada

--- cid-10/java/preprocessor/src/test/java/com/github/kyriosdata/bsus/cid10/preprocessor/IndiceTest.java
+++ cid-10/java/preprocessor/src/test/java/com/github/kyriosdata/bsus/cid10/preprocessor/IndiceTest.java
@@ -8,11 +8,11 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
-public class ConversorTest {
+public class IndiceTest {
 
     @Test
     public void montagemIndice() throws FileNotFoundException {
-        Map<String, Set<String>> idxWords = Conversor.montaIndice(Subcategorias.newInstance("cid-10-subcategorias-lower.json").descricao);
+        Map<String, Set<String>> idxWords = Indice.montaIndice(Subcategorias.newInstance("cid-10-subcategorias-lower.json").descricao);
         assertEquals(26 * 26, idxWords.size());
     }
 }
