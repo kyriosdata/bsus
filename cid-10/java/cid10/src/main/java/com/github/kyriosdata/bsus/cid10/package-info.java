321d56e3ccf271e33a9f39b66e76ad2e96b957c4
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2017-06-15 21:24:01 -0300

    documentacao

--- cid-10/java/cid10/src/main/java/com/github/kyriosdata/bsus/cid10/package-info.java
+++ cid-10/java/cid10/src/main/java/com/github/kyriosdata/bsus/cid10/package-info.java
@@ -1,5 +1,11 @@
 /**
  * Implementação de busca em códigos e/ou descrições da CID-10.
  *
+ * <p>Dois arquivos servem de entrada para a implementação:
+ * (a) cid10.org (arquivo contendo a CID-10 original, ou seja,
+ * com acentos e símbolos); (b) cid10.ser (arquivo contendo
+ * CID-10 sem acentos, sem símbolos especiais e outras
+ * "transformações" realizadas sobre o original com o propósito
+ * de simplificar o processo de busca.
  */
 package com.github.kyriosdata.bsus.cid10;
