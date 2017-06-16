4c32d23b1c00010e42b05fbb0d56cc1b0408f484
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2017-06-15 21:20:12 -0300

    mais alguns casos de teste

--- cid-10/java/cid10/src/test/java/com/github/kyriosdata/bsus/cid10/Cid10Test.java
+++ cid-10/java/cid10/src/test/java/com/github/kyriosdata/bsus/cid10/Cid10Test.java
@@ -4,6 +4,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class Cid10Test {
@@ -17,7 +18,7 @@ public class Cid10Test {
     }
 
     @Test
-    public void buscaPorPalavraInexistente() {
+    public void testeEntradaExistente() {
 
         final String[] criterios = {"y112", "intoxicacao", "anti" };
 
@@ -25,5 +26,25 @@ public class Cid10Test {
         assertEquals(1, identificadas.length);
 
         assertTrue(identificadas[0].contains("[intoxicação]"));
+    }
+
+    @Test
+    public void entradaInexistenteNaoPodeSerEncontrada() {
+        final String[] criterio = { "abcdefg" };
+        assertNull(cid.search(criterio));
+    }
+
+    @Test
+    public void algumasEntradasConhecidas() {
+        final String[] criterio = { "xxii" };
+        assertTrue(cid.search(criterio)[0].contains("XXII"));
+    }
+
+    @Test
+    public void outraBusca() {
+        final String[] criterio = { "al", "as", "cci", "os", "ar", "it", "um" };
+        String[] encontradas = cid.search(criterio);
+        assertEquals(1, encontradas.length);
+        assertTrue(encontradas[0].contains("A00-B99"));
     }
 }
